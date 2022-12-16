8380d9c923ee9d71b43da4532c9584bd06bd7f2c
Rishav <rishavsinha380@gmail.com>
2022-12-16 11:32:44 +0530

    User program

---
+++ src/User.js
@@ -0,0 +1,15 @@
+//functional component
+
+function User()
+{
+    // more than one tag only used inside div
+    return(
+        <div>
+        <h1>Functional Component</h1>
+        <h3>Namaste World</h3>
+        </div>
+    )
+}
+
+//export this file then import file wherever it needs.
+export default User;
