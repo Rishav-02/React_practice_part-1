ebc16890678f8de26734fdd1eb71a6bed0d7cd81
Rishav <rishavsinha380@gmail.com>
2022-12-16 11:30:41 +0530

    CLASS

---
+++ src/Class.js
@@ -0,0 +1,13 @@
+// class component
+
+import React,{Component} from "react";
+
+export default class Class extends Component
+{
+    render()
+    {
+        return(
+            <h1>Class Component</h1>
+        )
+    }
+}
